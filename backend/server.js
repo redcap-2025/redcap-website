bc2b86af60eacf0d0808f7b56f760596e92f939a
redcap-2025 <your-email@example.com>
2025-09-07 16:25:18 +0530

    update health check

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const LOCALHOSTS = [
   'http://localhost:3000',
   'http://127.0.0.1:5000',
   'http://localhost:49212',
+  'http://localhost:8081',
   
   
   'https://recapweb.netlify.app', // Your production frontend
